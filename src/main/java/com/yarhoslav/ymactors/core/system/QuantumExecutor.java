713b8c1e64726bb678e2996380b0162886e23fbe
yarhoslavme <yarhoslavme@gmail.com>
2017-10-29 21:47:36 -0400

    Transform to Version 2.  First steps.

---
+++ src/main/java/com/yarhoslav/ymactors/core/system/QuantumExecutor.java
@@ -0,0 +1,11 @@
+package com.yarhoslav.ymactors.core.system;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ *
+ * @author yarhoslavme
+ */
+public class QuantumExecutor extends ForkJoinPool {
+    //TODO: Manage the quantum execution and assign new ticket
+}
