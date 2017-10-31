36c839dcdbe9897b856ad24f7b23fc376833dc57
yarhoslavme <yarhoslavme@dev.lan>
2017-10-31 01:30:46 -0400

    Continue with new design

---
+++ src/main/java/com/yarhoslav/ymactors/core/system/QuantumTask.java
@@ -0,0 +1,18 @@
+package com.yarhoslav.ymactors.core.system;
+
+import java.util.concurrent.RecursiveAction;
+
+/**
+ *
+ * @author yarhoslavme
+ */
+public class QuantumTask extends RecursiveAction {
+
+    @Override
+    protected void compute() {
+        //TODO: Process one or more messages
+        //TODO: Before exit, check if still are messages in mailbox.  If there is more message then assign new ticket.
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
